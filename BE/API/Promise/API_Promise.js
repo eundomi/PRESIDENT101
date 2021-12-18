d4377954dc13c4b3f3175b8f4bcc18d900f303dd
asksue12 <jsy2015171@gmail.com>
2021-12-18 12:02:37 +0900

    :sparkles: [#11] promise api

---
+++ BE/API/Promise/API_Promise.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+const CandiPromise = require("../../models/Promise");
+const Party = require("../../models/Party");
+
+const asyncHandler = require("../../Middleware/asyncHandler");
+
+// 후보자 공약
+router.get("/:name", asyncHandler(async(req, res) => {
+    let { name } = req.params;
+    name = decodeURIComponent(name);
+
+    const { _id } = await Party.findOne({ name });
+    const promises = await CandiPromise.find({ candidate: _id });
+
+    res.status(200).json(promises);
+}));
+
+module.exports = router; 
