dad468bec7dd68e8170442343f4f727d685c4ac0
이의섭 <leeuiseop@gmail.com>
2021-12-18 17:27:37 +0900

    :memo: [#12] 쟁점이슈 좋아요 API 완성

--- BE/API/Like/API_Like.js
+++ BE/API/Like/API_Like.js
@@ -10,22 +10,34 @@ const router = express.Router();
 router.post("/upLike", auth, asyncHandler(async (req, res) => {
     const { shortId } = req.decoded;
     const { issueId } = req.body;
-    const like = await Like.create({ userId: shortId, issueId });
-    await Issue.updateOne({ id: issueId }, { $inc: { like: 1 }})
+    console.log(issueId);
+    const user = User.findOne({ shortId });
+    const like = await Like.create({ userId: user.id, issueId });
+    await Issue.updateOne({ _id: issueId }, { $inc: { like: 1 }})
     res.status(201).json({ msg: "해당 입장에 추천을 완료했습니다." });
 }));
 
 router.post("/unLike", auth, asyncHandler(async (req, res) => {
     const { shortId } = req.decoded;
     const { issueId } = req.body;
-    await Promise.all([Like.deleteOne({ userId: shortId, issueId }), Issue.updateOne({ id: issueId }, { $inc: { like: -1 }})])
+    const user = User.findOne({ shortId });
+    await Promise.all([Like.deleteOne({ $and: [{ userId: user.id, issueId }] }), Issue.updateOne({ _id: issueId }, { $inc: { like: -1 }})])
     res.status(201).json({ msg: "해당 입장에 추천을 취소했습니다." });
 }))
 
 router.get("/checkedList", auth, asyncHandler(async (req, res) => {
-    const { shortId } = req.body;
-    const checkedIssues = await Like.find({ userId: shortId });
+    const { shortId } = req.decoded;
+    const user = User.findOne({ shortId });
+    const checkedIssues = await Like.find({ userId: user.id });
     res.status(200).json(checkedIssues);
 }));
+
+router.delete("/delete", auth, asyncHandler(async (req, res) => {
+    const { shortId } = req.decoded;
+    const { issueId } = req.body;
+    const user = User.findOne({ shortId });
+    const what = await Like.deleteOne({ $and: [{ userId: user.id, issueId }] });
+    res.status(200).json({ msg: "삭제가 완료되어라 제발."});
+}))
 
 module.exports = router;
