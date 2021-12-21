dddd084b41802f0d92090605710094145e42359b
seoyoung-dev <seoyoung.dev@gmail.com>
2021-12-21 15:01:06 +0900

    💄 화면넓이 1000px 이하 미디어쿼리 작성

--- president101/app.js
+++ president101/app.js
@@ -12,9 +12,8 @@ getTime();
 function more() {
     let content = document.getElementById("videos__more__content");
     let btn = document.querySelector(".videos__more__btn");
-    if (content.style.display == "none") {
+    if (content.style.display == "") {
         content.style.display = "flex";
-        console.log("good");
         btn.remove();
     } else {
         content.style.display = "none";
