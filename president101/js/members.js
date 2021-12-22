33992a1f9b2d5ba2e5b42aee58bf58444a180ee2
seoyoung-dev <seoyoung.dev@gmail.com>
2021-12-23 02:06:38 +0900

    후보자 공약 내용 작성

--- president101/js/members.js
+++ president101/js/members.js
@@ -1,10 +1,10 @@
+// d-day countdown
 const dday = document.querySelector(".nav__d-day");
 function getTime() {
     const electionDay = new Date("2022-03-09:00:00:00+0900");
     const now = new Date();
     const gap = electionDay - now;
     const day = Math.floor(gap / (1000 * 60 * 60 * 24));
-
     dday.innerText = `D-${day}`;
 }
 
@@ -14,14 +14,13 @@ function init() {
 }
 init();
 
+// video more
 function more() {
     let content = document.getElementById("videos__more__content");
     let btn = document.querySelector(".videos__more__btn");
     if (content.style.display == "") {
         content.style.display = "flex";
         btn.remove();
-    } else {
-        content.style.display = "none";
     }
 }
 
@@ -32,18 +31,13 @@ toggleBtn.addEventListener("click", () => {
 });
 
 // api fetch
-
 const port = "elice-kdt-sw-1st-vm11.koreacentral.cloudapp.azure.com:5000";
-
-const info = {};
-
 const mainColor = {
     "%EC%9D%B4%EC%9E%AC%EB%AA%85": "#6399da",
     "%EC%9C%A4%EC%84%9D%EC%97%B4": "#df6962",
     "%EC%8B%AC%EC%83%81%EC%A0%95": "#ebbf5c",
     "%EC%95%88%EC%B2%A0%EC%88%98": "#e88c4e",
 };
-
 const profilePicture = {
     "%EC%9D%B4%EC%9E%AC%EB%AA%85": "../imgs/big-01.png",
     "%EC%9C%A4%EC%84%9D%EC%97%B4": "../imgs/big-02.png",
@@ -51,9 +45,36 @@ const profilePicture = {
     "%EC%95%88%EC%B2%A0%EC%88%98": "../imgs/big-04.png",
 };
 
+function getJobData(arr) {
+    let result = [];
+    for (const key of arr) {
+        result.push(key.desc);
+    }
+    return result;
+}
+
+function getPromiseData(arr) {
+    const conboxUl = document.querySelector(".conbox__ul");
+    for (const key of arr) {
+        let list = document.createElement("li");
+        let category = document.createElement("div");
+        category.className = "conbox__title";
+        category.innerText = key.category;
+        conboxUl.appendChild(list);
+        list.appendChild(category);
+        for (const promise of key.data) {
+            let content = document.createElement("div");
+            content.className = "conbox__content";
+            content.innerText = promise;
+            list.appendChild(content);
+        }
+    }
+
+    console.log(conboxUl);
+}
+
 let partyName = document.querySelector(".member__party_name");
 let memberName = document.querySelector(".member__name");
-
 let memberPhoto = document.querySelector(".member__image img");
 let memberBackgroundColor = document.querySelector(".members");
 
@@ -65,7 +86,6 @@ let propertyInfo = document.querySelector(".property dd");
 let armyInfo = document.querySelector(".army dd");
 let convictionInfo = document.querySelector(".conviction dd");
 
-let id;
 let encodedName = "";
 function fetchPage(name) {
     encodedName = encodeURIComponent(name);
@@ -77,24 +97,20 @@ function fetchPage(name) {
             birthInfo.innerText = data.birth;
             familyInfo.innerText = `${data.family}      ${data.companion}`;
             eduInfo.innerHTML = data.edu.join("<br>");
-            jobInfo.innerHTML = data.carrier.map;
+            jobInfo.innerHTML = getJobData(data.carrier).join("<br>");
             propertyInfo.innerText = data.property;
             armyInfo.innerText = data.army;
             convictionInfo.innerHTML = data.conviction.join("<br>");
             memberPhoto.setAttribute("src", profilePicture[name]);
         })
         .catch((err) => console.error(err));
+
+    fetch(`http://${port}/api/promise/${encodedName}`)
+        .then((res) => res.json())
+        .then((data) => {
+            getPromiseData(data);
+        })
+        .catch((err) => console.error(err));
 }
 
-// fetchPage("이재명");
 fetchPage(location.href.split("?")[1]);
-
-// function fetchPage(name) {
-//     encodedName = encodeURIComponent(name);
-//     fetch(`http://${port}/api/candidate/${encodedName}`)
-//         .then((res) => res.json())
-//         .then((data) => {
-//             obj = data;
-//         })
-//         .then(() => console.log(obj));
-// }
